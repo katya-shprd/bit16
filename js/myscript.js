5f22a38348cb2789a0d67196f5f1b96d31b3450c
alenkrp <32699591+alenkrp@users.noreply.github.com>
2017-12-12 15:56:36 +0200

    changed js, add animation

--- js/myscript.js
+++ js/myscript.js
@@ -5,50 +5,50 @@
 **/
 $(document).ready(function(){
 	
-	loadCompaniesDefault(); //loads the default list of companies
-	loadStoriesDefault();
-	loadActivitiesDefault(); 
-	$("#cmpTable").click(function(){
+	loadCompaniesDefault();
+
+	$("#compTable").click(function(){
 		loadCompaniesTable();
 	});
-	$("#search").keyup(function(){
+	$("#compSearch").keyup(function(){
 		searchCompanies();
 	});
-
-	$("#actTable").click(function(){
-		loadActivitiesTable();
-	});
-	$("#actsearch").keyup(function(){
-		searchActivity();
-	});
-
+	loadStoriesDefault();
+	
 	//searching stories in stories page - when searcword is written
 	$("#searchstory").keyup(function(){
 		searchStories();
 	});
-	
 	//searching stories in stories page - when storytype is changed
 	$("select").change(function(){
 		searchStories();
 	});
 
+	loadActivitiesDefault(); 
 
-	//when user clicks on the button "Read more" in storylist. Button is created with jQuery, so class "readmore" is used to get its value
-	$('#stories').on('click','.readmore', function(){
-		//the storyid from button is taken and passed to the query of stories to get selected story
-		$.getJSON( "selectedStory.php?storyID="+$(this).attr('data-storyID'), function( data ) {
-		var resultlist='';
-		$.each( data, function( key, story ) {
-			//data is shown in modal form (defined in index.php)
-			$("#storyTitle").html(story.storyTitle);
-			$("#storyDescription").html(story.storyDescription);
-			$("#storyLink").html('<iframe width="550" height="400" src="'+story.storyLink+'"></iframe>');
-		});		
-		
+	$("#actTable").click(function(){
+		loadActivitiesTable();
 	});
+	$("#actSearch").keyup(function(){
+		searchActivities();
 	});
-	
-	
+
+	$(".content").hide();
+	$("#comp").click(function(){
+		$("#actlist").hide();
+		$("#complist").show();
+	});
+	$("#act").click(function(){
+		$("#complist").hide();
+		$("#actlist").show();
+	});
+	$('#login-content').hide();
+    $('#login-trigger').click(function(){
+        $(this).next('#login-content').slideToggle();
+        $(this).toggleClass('active');					
+        if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+            else $(this).find('span').html('&#x25BC;')
+        })
 	$("#formsignin").submit(function(){
 		var msg;
 		$("#logininfo").html('<p class="alert alert-info"> Checking login...</p>');
@@ -80,77 +80,56 @@ $(document).ready(function(){
 		});
 		return false; //no reload on the page
 	});
+
+	myFunction();
 });
 
-function loadCompaniesDefault(){
- $.getJSON( "companiesDefault.php", function( data ) {
-	var resultlist='';
-	$.each( data, function( key, company ) {
-		resultlist=resultlist+company.companyID+' '+company.companyName+' '+company.city+'<br />';
-	});
-	$("#results").html(resultlist);
- });
-}
 
-function loadCompaniesTable(){
- var resulttable='<table class="table table-condensed"><thead><tr><th>#</th><th>Company</th><th>Address</th><th>About</th><th>Website</th><th>Facebook</th></tr></thead><tbody>';
- $.getJSON( "companiesDefault.php", function( data ) {
+function loadCompaniesDefault(){
+	$.getJSON( "companiesDefault.php", function( data ) {
 	var resultlist='';
 	$.each( data, function( key, company ) {
 		resultlist=resultlist+'<tr><td>'+company.companyID+'</td><td>'+company.companyName+'</td>';
 		resultlist=resultlist+'<td>'+company.street+', '+company.postnr+' '+company.city+'</td>';
 		resultlist=resultlist+'<td>'+company.description+'</td><td>'+company.website+'</td><td>'+company.facebook+'</td></tr>';
 	});
-	resulttable=resulttable+resultlist+'</tbody></table>';
-	$("#results").html(resulttable);
+	$("#compResults").html(resultlist);
  });
 }
 function searchCompanies(){
-	var searchword=$("#search").val();
+	var searchword=$("#compSearch").val();
 	$.getJSON("companySearch.php?search="+searchword,function(data){
 		var resultlist='';
 		$.each( data, function( key, company ) {
-			resultlist=resultlist+company.companyID+' '+company.companyName+' '+company.city+'<br />';
+			resultlist=resultlist+'<tr><td>'+company.companyID+'</td><td>'+company.companyName+'</td>';
+			resultlist=resultlist+'<td>'+company.street+', '+company.postnr+' '+company.city+'</td>';
+			resultlist=resultlist+'<td>'+company.description+'</td><td>'+company.website+'</td><td>'+company.facebook+'</td></tr>';
 		});
-	$("#results").html(resultlist);
+	$("#compResults").html(resultlist);
 	});
 }
 
-
-
 function loadActivitiesDefault(){
-	$.getJSON( "activitiesDefault.php", function( data ) {
-	   var resultlist='';
-	   $.each( data, function( key, activity ) {
-		   resultlist=resultlist+activity.activityID+' '+activity.activityName+' '+activity.activityKeyword+'<br />';
-	   });
-	   $("#activities").html(resultlist);
-	});
-   }
-   function loadActivitiesTable(){
-	var resulttable='<table class="table table-condensed"><thead><tr><th>#</th><th>Activity</th><th>Keyword</th><th>Description</th></tr></thead><tbody>';
-	$.getJSON( "activitiesDefault.php", function( data ) {
+	   $.getJSON( "activitiesDefault.php", function( data ) {
 	   var resultlist='';
 	   $.each( data, function( key, activity ) {
 		   resultlist=resultlist+'<tr><td>'+activity.activityID+'</td><td>'+activity.activityName+'</td>';
-		   resultlist=resultlist+'<td>'+activity.activityKeyword+', '+activity.activityDescription+'</td></tr>';
+		   resultlist=resultlist+'<td>'+activity.activityKeyword+'</td><td>'+activity.activityDescription+'</td></tr>';
 	   });
-	   resulttable=resulttable+resultlist+'</tbody></table>';
-	   $("#activities").html(resulttable);
+	   $("#actResults").html(resultlist);
 	});
-   }
-   function searchActivities(){
-	   var searchword=$("#search").val();
+}
+function searchActivities(){
+	   var searchword=$("#actSearch").val();
 	   $.getJSON("activitiesSearch.php?search="+searchword,function(data){
 		   var resultlist='';
 		   $.each( data, function( key, activity ) {
-			   resultlist=resultlist+activity.cactivityID+' '+activity.activityName+' '+activity.activityKeyword+'<br />';
+			resultlist=resultlist+'<tr><td>'+activity.activityID+'</td><td>'+activity.activityName+'</td>';
+			resultlist=resultlist+'<td>'+activity.activityKeyword+'</td><td>'+activity.activityDescription+'</td></tr>';
 		   });
-	   $("#activities").html(resultlist);
+	   $("#actResults").html(resultlist);
 	   });
-   }
-
-
+}
 
 function loadStoriesDefault(){
 	$.getJSON( "storiesDefault.php", function( data ) {
@@ -158,8 +137,7 @@ function loadStoriesDefault(){
 	$.each( data, function( key, story ) {
 		resultlist=resultlist+'<tr><td>'+story.storyTitle+'</td><td>'+story.storyType+'</td>';
 		resultlist=resultlist+'<td><a href="'+story.storyLink+'" target="_new">To the story</a></td>';
-		resultlist=resultlist+'<td><button type="button" class="readmore btn" data-toggle="modal" data-target="#showStory" ';
-		resultlist=resultlist+'data-storyID="'+story.storyID+'">Read more</button></td>';
+		resultlist=resultlist+'<td>'+story.storyKeywords+'</td><td>'+story.storyDescription+'</td>';
 		resultlist=resultlist+'</tr>';	
 	});
 	$("#stories").html(resultlist);
@@ -174,11 +152,18 @@ function searchStories(){
 	$.each( data, function( key, story ) {
 		resultlist=resultlist+'<tr><td>'+story.storyTitle+'</td><td>'+story.storyType+'</td>';
 		resultlist=resultlist+'<td><a href="'+story.storyLink+'" target="_new">To the story</a></td>';
-		resultlist=resultlist+'<td><button type="button" class="readmore btn" data-toggle="modal" data-target="#showStory" ';
-		resultlist=resultlist+'data-storyID="'+story.storyID+'">Read more</button></td>';
+		resultlist=resultlist+'<td>'+story.storyKeywords+'</td><td>'+story.storyDescription+'</td>';
 		resultlist=resultlist+'</tr>';	
 	});
 	$("#stories").html(resultlist);
  });
-	
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
